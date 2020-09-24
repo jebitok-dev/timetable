d5943d18608ba5168671d52b81a42020bb35d13a
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-24 23:33:32 +0300

    adding webpack, momentsjs & babel-loader

--- src/components/classes/class.component.jsx
+++ src/components/classes/class.component.jsx
@@ -22,10 +22,32 @@ class Class extends Component {
         }
     }
 
+    renderDays() {
+        return 
+            this.state.time.map((time, index) => {
+                const { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday } = time
+                return (
+                    <tr tr={Monday} >
+                        <td>{Monday}</td>
+                        <td>{Tuesday}</td>
+                        <td>{Wednesday}</td>
+                        <td>{Thursday}</td>
+                        <td>{Friday}</td>
+                        <td>{Saturday}</td>
+                    </tr>
+                )
+            })
+    }
+
     render() {
         return (
             <div>
-                <h1>Class</h1>
+                <h1 id="title">Class</h1>
+                <table id='students'>
+                    <tbody>
+                        {this.renderDays()}
+                    </tbody>
+                </table>
             </div>
         )
     }
